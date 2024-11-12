9c13c8b7677674cead5279d448d0493cd7446732
benKapl <benji1.kaplan@gmail.com>
2024-11-12 08:00:34 +0100

    started to add handleKeyDown

--- components/Bloc.js
+++ components/Bloc.js
@@ -4,13 +4,29 @@ import { useState } from 'react'
 const Bloc = ({ onClick }) => {
     const [inputValue, setInputValue] = useState('') 
 
+    const addBlock = () => {
+        onClick()
+    }
+
+    const handleChange = (event) => {
+        setInputValue(event.target.value)
+    }
+
+    const handleKeyDown = (event) => {
+        if (event.key === "Enter") {
+          console.log("Enter key was pressed");
+          // Add any additional actions here
+        }
+    }
+
     return (
         <div className={styles.bloc}
-        onClick={onClick}>
+        onClick={addBlock}>
             <input
                 className={styles.bloc}
                 type="text"
                 value={inputValue}
+                onKeyDown={handleKeyDown}
                 onChange={(e) => setInputValue(e.target.value)}>
             </input>
         </div>
