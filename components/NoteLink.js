63c862f0ad4dad286418314e783f83cbe979aa18
NicoJoub <105387349+NicoJoub@users.noreply.github.com>
2024-12-13 15:09:49 +0100

    add comment

--- components/NoteLink.js
+++ components/NoteLink.js
@@ -10,10 +10,13 @@ function NoteLinks({ title, noteId, isCurrent, stylePage }) {
     console.log('cliqué sur' + noteId);
   };
 
+  // variable pour le style de base dut titre de la note
   const baseStyle =
     'cursor-pointer transition duration-300 hover:text-lightPurple ease-in-out rounded-md text-darkPurple w-auto ml-1 mr-1 pl-2';
 
+  // variable qui permet de dynamiser en fonction de la page
   const styleAccordingToThePage = {
+    // nom de la page -> props ensuite pour dynamiser si note courante
     sidebar: isCurrent && 'bg-darkPurple text-white w-auto',
   };
 
