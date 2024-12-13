4ac986babb32fef5ff17ab658fe9cffacf19a590
Mohamed <mohamed.oulahssaine@gmail.com>
2024-12-13 12:22:14 +0100

    finished creation of compoment ConnectedUser - added an alt comment on the imported ConnectedUser in SidebarLeft component - it's working

--- components/SidebarLeft.js
+++ components/SidebarLeft.js
@@ -126,9 +126,7 @@ export default function SidebarLeft({ toggleSidebarLeft, createNote }) {
       </div>
 
       {/* Utilisation du comaposant ConnectedUser*/}
-        <ConnectedUser 
-        alt='Choose between Dark or Light mode'
-        />
+        <ConnectedUser />
       
     </div>
   );
