790660a5651c1af62b45aef7447c3bd3760a90c6
Marvin <timodentmarvin@live.fr>
2024-12-19 21:03:40 +0100

    add gif placeholder

--- components/Placeholder.js
+++ components/Placeholder.js
@@ -4,8 +4,8 @@ export default function Placeholder() {
   return (
     <div className='bg-whitePure border-solid border border-black p-3 rounded-lg'>
       <div className='mt-8 flex flex-col justify-center items-center h-[80vh]'>
-        <img src='noteviod.png' alt='spellbook' width={500} height={500} />
-        <p className='mt-8 text-darkPurple text-lg italic '>
+      <img src='mage.gif' alt='spellbook' width={550} height={550} />
+        <p className='text-darkPurple text-3xl italic '>
           Il est temps pour toi de créer une note
         </p>
       </div>
